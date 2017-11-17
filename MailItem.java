9c3056b9660b7afb3ece05315e9130113a30ff95
Lorena Alonso <lorenaalonsop26@gmail.com>
2017-11-17 09:59:43 +0100

    Método subject y cambios realizados

--- MailItem.java
+++ MailItem.java
@@ -13,7 +13,8 @@ public class MailItem
     private String to;
     // The text of the message.
     private String message;
-
+    //
+    private String subject;
     /**
      * Create a mail item from sender to the given recipient,
      * containing the given message.
@@ -21,13 +22,22 @@ public class MailItem
      * @param to The intended recipient of this item.
      * @param message The text of the message to be sent.
      */
-    public MailItem(String from, String to, String message)
+    public MailItem(String from, String to, String message, String subject)
     {
         this.from = from;
         this.to = to;
         this.message = message;
+        this.subject = subject;
     }
-
+    
+    /**
+     * Crear y enviar emails con asunto.
+     */
+    public String subject()
+    {
+        return subject;
+    }
+    
     /**
      * @return The sender of this message.
      */
@@ -60,5 +70,6 @@ public class MailItem
         System.out.println("From: " + from);
         System.out.println("To: " + to);
         System.out.println("Message: " + message);
+        System.out.println("Subject:" + subject);
     }
 }
